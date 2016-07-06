6b5dd430579480d0b5f0553467f5837abdb07940
Jaewe Heo <jaeweheo@gmail.com>
2016-07-06 20:48:36 +0900

    create table

--- table.js
+++ table.js
@@ -1,15 +1,133 @@
 'use strict';
 
-const RE_TABLE = /\|===\s+([^]*)\n\|===/u;
+const {
+  chunk,
+  indexOf,
+  range
+} = require('lodash');
 
-module.exports = input => {
+function parseMetadata(input) {
+  const result = {
+    cols: [1],
+    options: []
+  };
+
+  input.forEach(item => {
+    if (item[0] === 'cols') {
+      result.cols = item[1].split(/\s*,\s*/g).map(i => parseInt(i, 10));
+    } else if (item[0] === 'options') {
+      result.options = item[1].split(/\s*,\s*/g).map(i => i.trim());
+    }
+  });
+
+  const total = result.cols.reduce((prev, curr) => prev + curr, 0);
+  if (total > 0) {
+    result.cols = result.cols.map(col => Math.round((col / total) * 100));
+  }
+  return result;
+}
+
+function parse(input) {
+  const RE_TABLE = /(?:\[(.+)\])?\s+(\|===)\n((?:.*\n)*?)\2/mg;
+  const RE_METADATA = /(.+?)="(.+?)"(?:\s*,\s*|\s*$)/g;
+  const RE_SEPARATOR = /[ \t]*(?:([<^>]))?\|[ \t]+/;
+
   const match = RE_TABLE.exec(input);
   if (!match) {
     return null;
   }
 
-  const cells = match[1].replace(/^\|[ \t]+/, '').split(/\|[ \t]+/);
-  console.log(cells);
-  return input;
+  const rawMetadata = [];
+  let m = null;
+  while ((m = RE_METADATA.exec(match[1])) !== null) {
+    rawMetadata.push([m[1], m[2]]);
+  }
+
+  const metadata = parseMetadata(rawMetadata);
+
+  const cells = [];
+  let rawCells = match[3].trim().split(RE_SEPARATOR);
+  rawCells = rawCells.slice(1);
+
+  for (let i = 0; i < rawCells.length; i += 2) {
+    const sort = rawCells[i] || '<';
+    const text = rawCells[i + 1];
+    cells.push([sort, text]);
+  }
+
+  return {
+    metadata, cells
+  };
+}
+
+function getAlign(data) {
+  switch (data) {
+    case '>':
+      return 'right';
+    case '^':
+      return 'center';
+    default:
+      return 'left';
+  }
+}
+
+function getIndent(depth = 0, indent) {
+  return range(depth).map(() => indent || '').join('');
+}
+
+function renderHtml(metadata, cells, indent) {
+  const space = (!indent || indent === '') ? '' : indent;
+  const newLine = (!space || space === '') ? '' : '\n';
+  const hasHeader = indexOf(metadata.options, 'header') >= 0;
+  const numCols = metadata.cols.length;
+  const cols = metadata.cols.map(col => {
+    return [getIndent(2, space), '<col style="width: ', col, '%">'].join('');
+  }).join(newLine);
+  const colgroup = [
+    [space, '<colgroup>'].join(''),
+    cols, [space, '</colgroup>'].join('')
+  ].join(newLine);
+
+  const rows = chunk(cells, numCols).map((row, rowIndex) => {
+    const colItems = row.map(col => {
+      const align = getAlign(col[0]);
+      const tag = rowIndex === 0 && hasHeader ? 'th' : 'td';
+      return [
+        [getIndent(2, space), '<', tag, ' align="', align, '">'].join(''),
+        col[1].trim().replace(/\n/g, '<br/>'), ['</', tag, '>'].join('')
+      ].join('');
+    }).join(newLine);
+    return [
+      [space, '<tr>'].join(''),
+      colItems,
+      [space, '</tr>'].join('')
+    ].join(newLine);
+  }).join(newLine);
+
+  return [
+    '<table class="table" width="100%">',
+    colgroup,
+    rows,
+    '</table>'
+  ].join(newLine);
+}
+
+function renderJson(metadata, cells, indent) {
+  cells.forEach((cell, index) => {
+    cells[index] = {
+      align: getAlign(cell[0]),
+      text: cell[1]
+    };
+  });
+  const table = {
+    metadata, cells
+  };
+  return JSON.stringify(table, null, indent);
+}
+
+module.exports = {
+  parse,
+  renderHtml,
+  renderJson
 };
 
--- test.js
+++ test.js
@@ -1,11 +1,96 @@
 'use strict';
 
 import test from 'ava';
-import {table} from './';
+import {
+  table
+}
+from './';
 
 test('parse simple table', t => {
-  const input = `|===\n| english\n| 한글\n|===`;
-  const output = table(input);
-  t.true(input === output);
+  const input = `
+[cols="1,2",options="header"]
+|===
+
+>| a
+
+굳
+
+| b
+
+^| 하
+
+| 하
+
+|===
+`;
+  const output = table.parse(input);
+  const html = table.renderHtml(output.metadata, output.cells, '  ');
+  const expected = `<table class="table" width="100%">
+  <colgroup>
+    <col style="width: 33%">
+    <col style="width: 67%">
+  </colgroup>
+  <tr>
+    <th align="right">a<br/><br/>굳</th>
+    <th align="left">b</th>
+  </tr>
+  <tr>
+    <td align="center">하</td>
+    <td align="left">하</td>
+  </tr>
+</table>`;
+  t.is(html, expected);
+});
+
+
+test('convert table to json', t => {
+  const input = `
+[cols="1,2",options="header"]
+|===
+
+>| a
+
+굳
+
+| b
+
+^| 하
+
+| 하
+
+|===
+`;
+  const output = table.parse(input);
+  const json = table.renderJson(output.metadata, output.cells, '  ');
+  const expected = `{
+  "metadata": {
+    "cols": [
+      33,
+      67
+    ],
+    "options": [
+      "header"
+    ]
+  },
+  "cells": [
+    {
+      "align": "right",
+      "text": "a\\n\\n굳\\n\\n"
+    },
+    {
+      "align": "left",
+      "text": "b\\n\\n"
+    },
+    {
+      "align": "center",
+      "text": "하\\n\\n"
+    },
+    {
+      "align": "left",
+      "text": "하"
+    }
+  ]
+}`;
+  t.is(json, expected);
 });
 
