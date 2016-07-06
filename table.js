067d2475a2a3231c372fe2f24e7b9318ed1fb993
Jaewe Heo <jaeweheo@gmail.com>
2016-07-06 15:04:35 +0900

    cleanup

--- table.js
+++ table.js
@@ -4,11 +4,12 @@ const RE_TABLE = /\|===\s+([^]*)\n\|===/u;
 
 module.exports = input => {
   const match = RE_TABLE.exec(input);
-  if (match) {
-    const cells = match[1].replace(/^\| /, '').split(/\| /);
-    console.log(cells);
-    return input;
+  if (!match) {
+    return null;
   }
-  return null;
+
+  const cells = match[1].replace(/^\|[ \t]+/, '').split(/\|[ \t]+/);
+  console.log(cells);
+  return input;
 };
 
